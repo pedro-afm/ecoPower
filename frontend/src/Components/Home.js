b5baf4bdebf4658ca67a9ef7c3fda68de52fd817
pafm.iefp <pafm.iefp@alunos.ipb.pt>
2023-12-14 18:05:25 +0000

    ultimos

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -7,9 +7,13 @@ const Home = () => {
     return (
         <div>
             <Navbar />
-            <div style={{display:'flex'}} className="image-container">
-                <img style={{width:'100%', height:'70%'}} src={planoDeFundo} alt="Plano de Fundo" />
+            <div style={{ display: 'flex', position: 'relative' }} className="image-container">
+                <img style={{ width: '100%', height: '70%' }} src={planoDeFundo} alt="Plano de Fundo" />
                 <div className="overlay"></div>
+                <div style={{ position: 'absolute', top: '13%', left: '50%', transform: 'translate(-80%, -50%)', color: '#bbb', padding: '10px' }}>
+                    <h1 style={{color:'white', fontSize: '60px'}}>The future is coming</h1>
+                    <p>Descrição ou conteúdo adicional</p>
+                </div>
             </div>
         </div>
     );
